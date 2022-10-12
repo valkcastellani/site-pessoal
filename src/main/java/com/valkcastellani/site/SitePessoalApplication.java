3ca96cb2574c9f1cd11cea049fb61878230c54b3
Valk Castellani <valkcastellani@gmail.com>
2022-10-12 11:00:08 -0300

    Mas um teste

--- src/main/java/com/valkcastellani/site/SitePessoalApplication.java
+++ src/main/java/com/valkcastellani/site/SitePessoalApplication.java
@@ -1,18 +1,18 @@
 package com.valkcastellani.site;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 //(scanBasePackages = {"com.valkcastellani.site.controller"})
 //@Configuration
-//@EnableAutoConfiguration
+@EnableAutoConfiguration
 //@EnableConfigurationProperties
 //@EnableScheduling
 //@ComponentScan
-public class SitePessoalApplication 
-{  //extends SpringBootServletInitializer {
+public class SitePessoalApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SitePessoalApplication.class, args);
